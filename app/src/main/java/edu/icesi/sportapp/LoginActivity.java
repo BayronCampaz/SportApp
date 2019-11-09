6b1690caa795e5b03e112265e118cf1aae877c32
Bayron <bayrondaymir@yahoo.es>
2019-11-09 13:51:02 -0500

    Add Login with facebook, but doesn't work.

--- app/src/main/java/edu/icesi/sportapp/LoginActivity.java
+++ app/src/main/java/edu/icesi/sportapp/LoginActivity.java
@@ -2,13 +2,159 @@ package edu.icesi.sportapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = ">>>>";
+
+    private EditText emailEt;
+    private EditText passwordEt;
+    private Button signinBtn;
+    private Button signinFacebookBtn;
+    private LoginButton loginFacebook;
+    private TextView notUserYet;
+    FirebaseAuth auth;
+    CallbackManager mCallbackManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailEt = findViewById(R.id.email_login_et);
+        passwordEt = findViewById(R.id.password_login_et);
+        signinBtn = findViewById(R.id.signin_btn);
+        signinFacebookBtn = findViewById(R.id.signin_facebook_btn);
+        loginFacebook = findViewById(R.id.login_button);
+        notUserYet = findViewById(R.id.not_user_yet);
+
+        auth = FirebaseAuth.getInstance();
+        Log.e(">>>>>" ,"log esta funcionando");
+
+        signinBtn.setOnClickListener(
+                (v) -> {
+                    auth.signInWithEmailAndPassword(
+                            emailEt.getText().toString().trim(),
+                            passwordEt.getText().toString().trim()
+                    ).addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    });
+                }
+        );
+
+
+        signinFacebookBtn.setOnClickListener(view -> loginFacebook.performClick());
+
+        notUserYet.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SignupActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.e(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.e(TAG, "facebook:onCancel");
+                //updateUI(null);
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.e(TAG, "facebook:onError", error);
+               // updateUI(null);
+            }
+        });
+    }
+
+    /*@Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = auth.getCurrentUser();
+        //updateUI(currentUser);
+    }*/
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.e(TAG, "handleFacebookAccessToken:" + token);
+        // [START_EXCLUDE silent]
+        //showProgressDialog();
+        // [END_EXCLUDE]
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.e(TAG, "signInWithCredential:success");
+                            FirebaseUser user = auth.getCurrentUser();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                            return;
+
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                        //hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
     }
 }
